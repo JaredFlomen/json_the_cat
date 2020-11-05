7376577a041903c8d1ecc7f8fb2e6543fb3cb04f
Jared <jaredflomen@gmail.com>
2020-11-05 19:44:53 +0000

    Updating

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,17 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, 'The breed does not exist');
+    } else {
+      callback(null, data[0]['description']);
+    }
+  });
+}
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log("error", error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('BREED DOESNOT EXIST');
-  } else {
-    console.log(data[0]['description']);
-    console.log(typeof data);
-  }
-});
+module.exports = fetchBreedDescription;
